abb3023c956652efe9df1d5d4c9d11cdcd53443f
My Name <Me@gmail.com>
2022-10-12 19:52:35 +0400

    figured out the shit about index

--- Programming_task_4.py
+++ Programming_task_4.py
@@ -2,10 +2,17 @@
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 n = int(input('Введите число: '))
 numbers = list(range(-n, n+1))
-# for i in range(-n, n+1):
-    # print(i)
-string = str(numbers)
-print(numbers)
-with open('file.txt', 'w') as positions:
-    positions.writelines(string)
-print(positions)
+file = open('file.txt', 'w')
+for i in numbers:
+    string = str(numbers[i-n-1])
+    file.writelines(f'{string}\n')
+    print(string)
+
+    
+    
+    
+# string = str(numbers)
+# print(numbers)
+# for i in numbers:
+    # print(string[23])
+# print(positions)
