f40cf92914868e2644d7b74cf2878699c4629732
My Name <Me@gmail.com>
2022-10-12 23:13:10 +0400

    finished

--- Programming_task_2.py
+++ Programming_task_2.py
@@ -1,8 +1,11 @@
 # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N
+
 n = int(input('Введите число: '))
 list = list(range(1, n+1))
 result = 1
+
 print(f'Набор произведений чисел от 1 до {n}: ')
+
 for i in list:
     result = result * list[i-1]
     print(result)
